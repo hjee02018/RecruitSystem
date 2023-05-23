037c4eb5bd568f79d36244d2c9da1aece7544a0d
gyucheol <balance0591@naver,com>
2023-05-23 17:59:36 +0900

    recruitList 얻는 로직 수정

--- ver_2/implementation/control/Search.cpp
+++ ver_2/implementation/control/Search.cpp
@@ -7,16 +7,18 @@
  * @return matching company
  * @throws runtime_error If it can't find a matching company <- "예외 처리"
  */
+
 RecruitInfo Search::showRecruitmentInfoByCompanyName(string company_name)
 {
-    /**
-     어떻게 얻어올지....?
-     */
-    vector<RecruitInfo> recruit_info_dao; // 저장된 채용 정보 리스트 ? 모든 회사직원들중 반복하면서 찾아야하나?
     
+    vector<RecruitInfo> recruit_info_list;
     
-    for(int i =0; i<recruit_info_dao.size(); i++){
-        if(recruit_info_dao[i].getCompanyName() == company_name)
+    for(int i=0; i<numComp; i++){
+        recruit_info_list.push_back(comp[i].getRecruitment());
+    }
+        
+    for(int i =0; i<recruit_info_list.size(); i++){
+        if(recruit_info_list[i].getCompanyName() == company_name)
         {
             return recruit_info_dao[i];
         }
