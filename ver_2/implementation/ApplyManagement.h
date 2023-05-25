d1b4d1602d5ee1896d6375ab99564dfe264c79d7
HJYANG <78251977+hjee02018@users.noreply.github.com>
2023-05-26 05:57:05 +0900

    Update ApplyManagement.h

--- ver_2/implementation/ApplyManagement.h
+++ ver_2/implementation/ApplyManagement.h
@@ -1,21 +1,88 @@
-#ifndef ApplyInfoCollection_hpp
-#define ApplyInfoCollection_hpp
-
-#include <stdio.h>
-#include <vector>
-#include "ApplyInfo.h"
+#pragma once
+#include<iostream>
+#include<string.h>
+#include<fstream>
+#include <list>
+#include <map>
+#include "MemberManagement.h"
 
 using namespace std;
 
-/**
- * Represents a ApplyInfo Collection <- "클래스의 역할"
- *
- */
-class ApplyInfoCollection
+FILE* in_fp, * out_fp;
+
+#define MAX_STRING 32
+#define MAX_ACCOUNT 100
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+
+class CancelApplyInfo
 {
 private:
-    vector<ApplyInfo> m_recruit_applys;
+	GenMember m_genMember;
 public:
-    void addApplyInfo(ApplyInfo apply_info);
+	void cancelApplcyInfo(string compName, int compNo, string departName);
 };
-#endif /* ApplyInfoCollection_hpp */
+
+class GetApplyCount
+{
+private:
+	GenMember m_genMember;
+public:
+	void getApplyCount();
+};
+
+class GetRecruitStatus
+{
+private:
+	CompMember m_compMember;
+public:
+	void getRecruitStatus();
+};
+
+class ShowApplyInfo
+{
+private:
+	GenMember m_genMember;
+public:
+	void showApplyInfo();
+};
+
+class CancelApplyInfoUI
+{
+private:
+	CancelApplyInfo m_cancelApplyInfo;
+public:
+	void startInterface();
+	void cancelApplyInfo();
+};
+
+class GetApplyCountUI
+{
+private:
+	GetApplyCount m_getApplyCount;
+public:
+	void startInterface();
+	void getApplyCount();
+};
+
+class ShowApplyInfoUI
+{
+private:
+	ShowApplyInfo m_showApplyInfo;
+public:
+	void showApplyInfo();
+	void startInterface();
+};
+
+
+class GetRecruitStatusUI
+{
+private:
+	GetRecruitStatus m_getRecruitStats;
+public:
+	void startInterface();
+	void getRecruitStatus();
+};
+
+
