ab4694658c5497e808a1cd997b1ef8c15de9754f
HJYANG <78251977+hjee02018@users.noreply.github.com>
2023-05-26 05:56:38 +0900

    Update ApplyManagement.h

--- ver_2/implementation/ApplyManagement.h
+++ ver_2/implementation/ApplyManagement.h
@@ -1,114 +1,21 @@
-#pragma once
-#include<iostream>
-#include<string.h>
-#include<fstream>
-#include <list>
-#include <map>
-#include "MemberManagement.h"
+#ifndef ApplyInfoCollection_hpp
+#define ApplyInfoCollection_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "ApplyInfo.h"
 
 using namespace std;
 
-FILE* in_fp, * out_fp;
-
-#define MAX_STRING 32
-#define MAX_ACCOUNT 100
-#define INPUT_FILE_NAME "input.txt"
-#define OUTPUT_FILE_NAME "output.txt"
-
-
-class ApplyInfo
+/**
+ * Represents a ApplyInfo Collection <- "클래스의 역할"
+ *
+ */
+class ApplyInfoCollection
 {
 private:
-	string m_companyName;
-	string m_departName;
-	int m_headCount;
-	time_t m_endDate;
+    vector<ApplyInfo> m_recruit_applys;
 public:
-	string getDepart();
-	void showInfo();
-	~ApplyInfo();
+    void addApplyInfo(ApplyInfo apply_info);
 };
-
-class RecruitInfo
-{
-private:
-	string m_companyName;
-	string m_departName;
-	int m_numOfApply;
-	time_t m_endDate;
-public:
-	int getRecruitCount();
-	string getDepartName();
-};
-
-
-class CancelApplyInfo
-{
-private:
-	GenMember m_genMember;
-public:
-	void cancelApplcyInfo(string compName, int compNo, string departName);
-};
-
-class GetApplyCount
-{
-private:
-	GenMember m_genMember;
-public:
-	void getApplyCount();
-};
-
-class GetRecruitStatus
-{
-private:
-	CompMember m_compMember;
-public:
-	void getRecruitStatus();
-};
-
-class ShowApplyInfo
-{
-private:
-	GenMember m_genMember;
-public:
-	void showApplyInfo();
-};
-
-class CancelApplyInfoUI
-{
-private:
-	CancelApplyInfo m_cancelApplyInfo;
-public:
-	void startInterface();
-	void cancelApplyInfo();
-};
-
-class GetApplyCountUI
-{
-private:
-	GetApplyCount m_getApplyCount;
-public:
-	void startInterface();
-	void getApplyCount();
-};
-
-class ShowApplyInfoUI
-{
-private:
-	ShowApplyInfo m_showApplyInfo;
-public:
-	void showApplyInfo();
-	void startInterface();
-};
-
-
-class GetRecruitStatusUI
-{
-private:
-	GetRecruitStatus m_getRecruitStats;
-public:
-	void startInterface();
-	void getRecruitStatus();
-};
-
-
+#endif /* ApplyInfoCollection_hpp */
