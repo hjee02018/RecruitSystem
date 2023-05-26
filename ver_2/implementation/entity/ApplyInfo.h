ee376d74671615340699a9e374c37bdf49c23ed4
HJYANG <78251977+hjee02018@users.noreply.github.com>
2023-05-26 09:28:47 +0900

    [FIX] add class comment

--- ver_2/implementation/entity/ApplyInfo.h
+++ ver_2/implementation/entity/ApplyInfo.h
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std;
 
-
+/**
+ * 일반 회원의 지원 정보를 관리하는 클래스로 지원 정보(회사명,업무명,사업자번호,인원수,마감일)와 함수를 제공
+*/
 class ApplyInfo
 {
 private:
@@ -14,11 +16,10 @@ private:
 public:
 	ApplyInfo(string company_name, int business_number, string task);
 	string getDepart();
-	void showInfo(FILE* out_fp);
 	string getCompanyName();
 	string getTask();
 	string getCompany();
 	int getNumber();
 	time_t getDeadline();
-    int getCount();
+	int getCount();
 };
